f9d7610f119c653837258a221558037f0a8d2ac5
Umut Burdur <euburdur28@gmail.com>
2021-07-25 17:21:59 +0300

    First upload

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # Taximeter
-This is a practice at Patika.dev
+This is a practice at [Patika.dev](https://app.patika.dev/egitimler/java-ile-backend-web-development-patikasi/java101/pratik-taksimetre)
+
+This program will calculate the charge of a Taxi. Details can be seen below;
+
+- Charge per KM is 2.20 TL/KM.
+- Minimum charge of Taximeter is 20 TL.
+- Taximeter will start from 10 TL as default start charge.
+
---
+++ taximeter/src/Main.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        double km;
+        double perKm = 2.20, total = 10;
+        Scanner input = new Scanner(System.in);
+        System.out.print("How far did you go in km? : ");
+        km = input.nextDouble();
+        total+=(km*perKm);
+        total= (total < 20) ? 20: total;
+        System.out.println("Total charge is " + total);
+    }
+}
